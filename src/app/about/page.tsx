962046d2b90438344704cdbe7d0be849fef4cd4f
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-24 20:07:39 -0300

    feat: react zeloimoveis page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,94 @@ import style from './style.min.module.css';
 
 const About = () => {
   return (
-    <main id={style.main_about}>
-      <h2>Pagina sobre....</h2>
-    </main>
+    <>
+      <header id={style.header_about}>
+        <h2>Sobre a Zelo Imóveis</h2>
+      </header>
+      <main id={style.main_about}>
+        <section>
+          <h2>Ajudamos você a ter qualidade de vida morando em Campinas</h2>
+          <h3>Conheça um pouco mais da nossa história:</h3>
+          <p>
+            Experiência adquirida desde 1976 pelo Fundador Rubens Jacintho de
+            Campos que com sua visão de mercado fundou uma empresa com os
+            valores fundamentados na família, respeito e satisfação dos
+            clientes.
+          </p>
+          <p>
+            Com o falecimento do patrono em 1986, então seu filho Rubens Júnior
+            que trabalhou por muitos anos como químico industrial, engenheiro
+            mecânico e de segurança como Rhodia, Shell e CBI, assumiu o comando
+            da empresa com muito esforço, dedicação e sabedoria para manter o
+            mesmo padrão de excelência deixado pelo seu pai.
+          </p>
+          <p>
+            A partir de 2002, Felipe Moraes Campos com formações nas áreas de
+            direito e administração de empresas, juntamente com seu irmão
+            Christiano Moraes Campos, formado em Economia e Ciências Contábeis,
+            iniciaram a jornada de crescimento e juntaram forças e abraçaram o
+            desafio para a inserção de novas metodologias e inovações a uma
+            empresa com tradição na cidade de Campinas fortalecendo ainda mais a
+            organização.
+          </p>
+          <h3>Nossa Empresa</h3>
+          <p>
+            A Zelo imóveis atua a mais de 45 anos com seriedade e transparência
+            com vasta experiência em locação, vendas e administração de imóveis,
+            proporcionando a seus clientes atendimento personalizado e agilidade
+            nos serviços prestados.
+          </p>
+          <p>
+            A Zelo se empenha em profissionalismo tanto para quem procura um
+            imóvel como para os clientes proprietários.
+          </p>
+          <p>
+            A Zelo Imóveis almeja conquistar cada vez mais clientes, ser
+            referência no mercado investindo em melhorias sempre e
+            proporcionando qualidade em seus serviços.
+          </p>
+          <h3>Missão da Zelo:</h3>
+          <p>
+            Ser a solução de qualidade em serviços imobiliários – Locação
+            residencial e comercial, intermediação de vendas, para atender e
+            surpreender às expectativas de nossos clientes, diferenciando-se da
+            concorrência pela excelência no atendimento, construída há mais de 4
+            décadas a partir do fortalecimento do compromisso entre nós, nossos
+            colaboradores e parceiros.
+          </p>
+          <h3>Visão da Zelo:</h3>
+          <p>
+            Continuar sendo reconhecida no mercado imobiliário, como umas das
+            melhores empresas em administração de imóveis, locações e vendas,
+            comprometida sempre com a ética profissional, com a presteza nos
+            serviços e com a plena satisfação de nossos clientes internos e
+            externos sendo sempre parceira dos negócios.
+          </p>
+          <h3>Valores da Zelo:</h3>
+          <p>
+            Qualidade e eficácia – Sempre entregar o que oferecemos com
+            qualidade e presteza.
+          </p>
+          <p>
+            Respeito às pessoas – Valorizar, apoiar e reconhecer os nossos
+            colaboradores, tratando-os com respeito, educação e atenção sem
+            distinção de clientes, fornecedores e parceiros.
+          </p>
+          <p>
+            Compromisso – Cumprir com as obrigações e compromissos assumidos
+            zelando sempre pela responsabilidade e qualidade nos serviços
+            prestados.
+          </p>
+          <p>
+            Sustentabilidade – Usar conscientemente os recursos, evitando
+            desperdícios e ter a consciência de não gerar despesas
+            desnecessárias, visando a responsabilidade social e ambiental,
+            rentabilizando os negócios e não onerando nossos clientes e a
+            empresa.
+          </p>
+        </section>
+      </main>
+    </>
   );
 };
 
