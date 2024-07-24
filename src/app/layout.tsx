6174f1d9fc9eafcf243952eed821398a1c30f777
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-24 16:05:35 -0300

    feat: react zeloimoveis layout project

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+
+import { Lato } from 'next/font/google';
+
+import NavBar from '@/components/NavBar';
+import Footer from '@/components/Footer';
+
+import { Suspense } from 'react';
+
+import './globals.css';
+
+const lato = Lato({ subsets: ['latin'], weight: ['400', '700', '900'] });
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+  keywords: '',
+  icons: '',
+  robots: 'index, follow',
+  authors: [{ name: 'Eliel Silva', url: 'https://github.com/Eliel-Silva-dev' }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={lato.className}>
+        <NavBar />
+        <Suspense fallback={<div>Carregando dados da pagina...</div>}>
+          {children}
+        </Suspense>
+        <Footer />
+      </body>
+    </html>
+  );
+}
