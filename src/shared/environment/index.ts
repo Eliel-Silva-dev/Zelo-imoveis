538845d579c9b759840d523be8ee1d2db28a2a63
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-11 21:26:32 -0300

    feat: react zeloimoveis environment

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,10 @@
+export const Environment = {
+  /*
+    Finalidade do imovel: comprar ou alugar;
+  */
+  FINALIDADE_IMOVEL: ['Comprar', 'Alugar'],
+  /*
+    url base das consultas api
+  */
+  URL_BASE: 'http://localhost:3333',
+};
