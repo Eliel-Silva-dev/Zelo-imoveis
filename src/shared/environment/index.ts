05492484e386945d8f8f2e249d39f1e452783dc6
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-12 21:33:11 -0300

    feat: react zeloimoveis imoveis services getAllImoveis

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -4,6 +4,10 @@ export const Environment = {
   */
   FINALIDADE_IMOVEL: ['Comprar', 'Alugar'],
   /*
+    limite de dados retornados na busca:
+  */
+  LIMITE_BUSCA_API: 10,
+  /*
     url base das consultas api
   */
   URL_BASE: 'http://localhost:3333',
--- src/shared/services/api/alugar/ImoveisServices.ts
+++ src/shared/services/api/alugar/ImoveisServices.ts
@@ -1,5 +1,5 @@
-import { Environment } from "@/shared/environment";
-import { Api } from "../axios-config";
+import { Environment } from '@/shared/environment';
+import { Api } from '../axios-config';
 
 export interface IListagemImoveis {
   id: string;
@@ -18,4 +18,33 @@ type TImoveisComTotalCount = {
   totalCount: number;
 };
 
-export {alugarServices};
+const getAllImoveis = async (
+  page = 1,
+  filter = '',
+  id = '',
+): Promise<TImoveisComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/imoveis?_page=${page}&_limit=${Environment.LIMITE_BUSCA_API}&nome_like=${filter}&id_like=${id}`;
+
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers['x-total-count'] || Environment.LIMITE_BUSCA_API,
+        ),
+      };
+    }
+
+    return new Error('Erro ao listar os registro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao listar os registros',
+    );
+  }
+};
+
+export { getAllImoveis };
