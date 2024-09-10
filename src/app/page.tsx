a039c6810950f4500b51e9c2400300573d9ce650
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-10 17:05:32 -0300

    feat: react zelo imoveis secao best imoveis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import style from './page.module.css';
+import CardImovel from '@/shared/components/CardImovel';
 
 export default function Home() {
+  const [typeOfImovel, setTypeOfImovel] = useState<string>('comprar');
+
+  const changeTypeImovel = (typeImovel: string) => {
+    console.log('mostrando tipo de imovel: ', typeImovel);
+
+    setTypeOfImovel(typeImovel);
+  };
+
+  useEffect(() => {
+    //fazer uma requisição ao banco de dados.
+    // uma vez ao carregar a página, e sempre que o valor do tipo de imovel mudar.
+  }, [typeOfImovel]);
+
   return (
     <main id={style.main_home}>
       <h2>Referência em locação e vendas em Campinas</h2>
@@ -70,6 +87,34 @@ export default function Home() {
           <button type="submit">BUSCAR IMÓVEIS</button>
         </div>
       </form>
+      <section className={style.container_best_imovel}>
+        <h2>Imóveis em destaque</h2>
+        <div>
+          <span
+            className={style.type_imovel}
+            onClick={() => changeTypeImovel('comprar')}
+          >
+            COMPRAR
+          </span>
+          <span
+            className={style.type_imovel}
+            onClick={() => changeTypeImovel('alugar')}
+          >
+            ALUGAR
+          </span>
+        </div>
+        <div className={style.container_carrossel_best}>
+          <div className={style.carrossel_best}>
+            <CardImovel />
+            <CardImovel />
+            <CardImovel />
+          </div>
+          <div className={style.controls_carrossel}>
+            <span className={style.controls_btn}>{'<'}</span>
+            <span className={style.controls_btn}>{'>'}</span>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
