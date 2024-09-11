9076d1da65f4784d3d6c503e2e4af048160735fd
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-10 21:52:56 -0300

    feat: react zelo imoveis home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { ImoveisServices } from '@/shared/services/api';
 export default function Home() {
   const [finalidadeOfImovel, setTypeOfImovel] = useState<string>('compra');
   const [imovels, setImovels] = useState<IListagemImoveis[]>();
+
   const changeTypeImovel = (typeImovel: string) => {
     console.log('mostrando tipo de imovel: ', typeImovel);
 
@@ -27,88 +28,74 @@ export default function Home() {
 
   return (
     <main id={style.main_home}>
-      <h2>Referência em locação e vendas em Campinas</h2>
-      <form className={style.form_home}>
-        <div>
-          <label>Finalidade</label>
+      <section className={style.header}>
+        <h2>Referência em locação e vendas em Campinas</h2>
+        <form className={style.form_home}>
           <div>
-            <select id="finalidade">
-              <option value="1">Alugar</option>
-              <option value="2">Comprar</option>
-            </select>
+            <label>Finalidade</label>
+            <div>
+              <select id="finalidade">
+                <option value="1">Alugar</option>
+                <option value="2">Comprar</option>
+              </select>
+            </div>
           </div>
-        </div>
-
-        <div>
-          <label>Tipo</label>
           <div>
-            <select id="tipo">
-              <option value="0">Carregando...</option>
-            </select>
+            <label>Tipo</label>
+            <div>
+              <select id="tipo">
+                <option value="0">Carregando...</option>
+              </select>
+            </div>
           </div>
-        </div>
-
-        <div>
-          <label>Localização</label>
           <div>
-            <input
-              id={style.endereco}
-              type="text"
-              placeholder="Bairro ou Cidade"
-            />
+            <label>Localização</label>
+            <div>
+              <input
+                id={style.endereco}
+                type="text"
+                placeholder="Bairro ou Cidade"
+              />
+            </div>
+            <ul id="lista-endereco"></ul>
           </div>
-          <ul id="lista-endereco"></ul>
-        </div>
-
-        <div>
-          <label>Quartos</label>
           <div>
-            <select id="quartos">
-              <option value="0">Quartos</option>
-              <option value="1-quartos">1 Quarto</option>
-              <option value="2-quartos">2 Quartos</option>
-              <option value="3-quartos">3 Quartos</option>
-              <option value="4-quartos">4+ Quartos</option>
-            </select>
+            <label>Quartos</label>
+            <div>
+              <select id="quartos">
+                <option value="0">Quartos</option>
+                <option value="1-quartos">1 Quarto</option>
+                <option value="2-quartos">2 Quartos</option>
+                <option value="3-quartos">3 Quartos</option>
+                <option value="4-quartos">4+ Quartos</option>
+              </select>
+            </div>
           </div>
-        </div>
-
-        <div>
-          <label>Valor</label>
           <div>
-            <input id={style.input_valor_min} type="text" placeholder="Min" />
-            <input id={style.input_valor_max} type="text" placeholder="Max" />
+            <label>Valor</label>
+            <div>
+              <input id={style.input_valor_min} type="text" placeholder="Min" />
+              <input id={style.input_valor_max} type="text" placeholder="Max" />
+            </div>
           </div>
-        </div>
-
-        <div id="container-cond">
-          <label>Condomínios</label>
-          <div>
-            <select id="condominio">
-              <option value="0">Condomínio</option>
-            </select>
+          <div id="container-cond">
+            <label>Condomínios</label>
+            <div>
+              <select id="condominio">
+                <option value="0">Condomínio</option>
+              </select>
+            </div>
           </div>
-        </div>
-
-        <div id={style.submit_busca}>
-          <button type="submit">BUSCAR IMÓVEIS</button>
-        </div>
-      </form>
+          <div id={style.submit_busca}>
+            <button type="submit">BUSCAR IMÓVEIS</button>
+          </div>
+        </form>
+      </section>
       <section className={style.container_best_imovel}>
         <h2>Imóveis em destaque</h2>
-        <div>
-          <span
-            className={style.type_imovel}
-            onClick={() => changeTypeImovel('compra')}
-          >
-            COMPRAR
-          </span>
-          <span
-            className={style.type_imovel}
-            onClick={() => changeTypeImovel('aluga')}
-          >
-            ALUGAR
-          </span>
+        <div className={style.type_imovel}>
+          <span onClick={() => changeTypeImovel('compra')}>COMPRAR</span>
+          <span onClick={() => changeTypeImovel('aluga')}>ALUGAR</span>
         </div>
         <div className={style.container_carrossel_best}>
           <div className={style.carrossel_best}>
